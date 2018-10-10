d7720e4c158300d4685a4ff1da0420d7dbf85114
erosarrow <bakeryofsing@gmail.com>
2018-10-10 11:53:26 -0400

    Collection View

---
+++ Week1_Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Week1_Flix
+//
+//  Created by Sing Fung Cheung on 10/10/18.
+//  Copyright © 2018 Sing Fung Cheung. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
